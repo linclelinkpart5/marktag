afa6f86805779d8ce25dc2aef9435b76a6f8af7c
Mark LeMoine <linclelinkpart5@gmail.com>
2020-09-21 00:36:10 -0700

    Added basic `clap` usage

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,13 @@
+use clap::Clap;
+
+#[derive(Clap)]
+struct Opts {
+    #[clap(long)]
+    emit_existing: bool,
+    #[clap(long)]
+    normalize: bool,
+}
+
 fn main() {
+    let opts = Opts::parse();
 }
