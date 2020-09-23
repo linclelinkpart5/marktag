c56f4fb592da8fd17e1d1793e3278d888723372a
Mark LeMoine <linclelinkpart5@gmail.com>
2020-09-22 23:16:22 -0700

    Actually filtering FLAC files, and reading tags

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 
+use std::ffi::OsStr;
 use std::path::{Path, PathBuf};
 use std::collections::{HashMap, HashSet};
 
 use clap::Clap;
+use metaflac::Tag;
 
 #[derive(Debug, Clap)]
 struct Opts {
@@ -27,18 +29,24 @@ fn collect_entries(source_dir: &Path) -> Vec<Entry> {
         .read_dir()
         .unwrap()
         .map(Result::unwrap)
+        .map(|e| e.path())
+        .filter(|p| p.extension() == Some(OsStr::new("flac")))
         .collect::<Vec<_>>()
     ;
 
     let mut expected_track_nums = (0..flac_files.len()).collect::<HashSet<_>>();
 
     for flac_file in flac_files {
+        println!("{}", flac_file.display());
+        let flac_tag = Tag::read_from_path(&flac_file).unwrap();
     }
 
-    unimplemented!()
+    Vec::new()
 }
 
 fn main() {
     let opts = Opts::parse();
     println!("{:?}", opts);
+
+    collect_entries(&opts.source_dir);
 }
