d9deb675450d2c6a6503f3964b462472834f55fa
Mark LeMoine <linclelinkpart5@gmail.com>
2020-09-22 22:31:31 -0700

    Added positional args and modified flags

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
+
+use std::path::PathBuf;
+
 use clap::Clap;
 
-#[derive(Clap)]
+#[derive(Debug, Clap)]
 struct Opts {
+    source_dir: PathBuf,
+    album_block_file: PathBuf,
+    track_blocks_file: PathBuf,
     #[clap(long)]
     emit_existing: bool,
     #[clap(long)]
-    normalize: bool,
+    output_dir: Option<PathBuf>,
 }
 
 fn main() {
     let opts = Opts::parse();
+    println!("{:?}", opts);
 }
