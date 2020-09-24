acdee6b87f120acd9604eeb82ec8f042f86d00c9
Mark LeMoine <linclelinkpart5@gmail.com>
2020-09-24 16:04:20 -0700

    Bulk of work on `collect_entries` complete, just need to store entries and validate

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::collections::{HashMap, HashSet};
 
 use clap::Clap;
 use metaflac::Tag;
+use metaflac::block::VorbisComment;
 
 #[derive(Debug, Clap)]
 struct Opts {
@@ -20,7 +21,18 @@ struct Opts {
 struct Entry {
     path: PathBuf,
     track_num: usize,
-    block: HashMap<String, Vec<String>>,
+    block: VorbisComment,
+}
+
+fn expect_one<T>(it: impl Iterator<Item = T>) -> T {
+    let mut it = it.into_iter();
+    let first = it.next();
+    let second = it.next();
+
+    match (first, second) {
+        (Some(e), None) => e,
+        _ => panic!("did not find exactly one value"),
+    }
 }
 
 fn collect_entries(source_dir: &Path) -> Vec<Entry> {
@@ -35,13 +47,27 @@ fn collect_entries(source_dir: &Path) -> Vec<Entry> {
     ;
 
     let mut expected_track_nums = (0..flac_files.len()).collect::<HashSet<_>>();
+    let mut entries = Vec::with_capacity(flac_files.len());
 
     for flac_file in flac_files {
         println!("{}", flac_file.display());
         let flac_tag = Tag::read_from_path(&flac_file).unwrap();
+
+        let track_num_str = expect_one(flac_tag.get_vorbis("tracknumber").unwrap());
+        let track_num = track_num_str.parse::<usize>().unwrap();
+
+        assert!(expected_track_nums.remove(&track_num), "unexpected track number");
+
+        let block = flac_tag.vorbis_comments().unwrap().clone();
+
+        let entry = Entry {
+            path: flac_file,
+            track_num,
+            block,
+        };
     }
 
-    Vec::new()
+    entries
 }
 
 fn main() {
