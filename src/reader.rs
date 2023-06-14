795d8324edf9e1650aace0d752f67e51176c58a3
Mark LeMoine <linclelinkpart5@gmail.com>
2023-06-14 15:08:31 -0700

    Revamp track collection WIP

--- src/reader.rs
+++ src/reader.rs
@@ -58,8 +58,8 @@ pub(crate) fn load_split_metadata(album_path: &Path, track_path: &Path) -> Metad
     }
 }
 
-pub(crate) fn emit_existing_tags(
-    tags: impl Iterator<Item = Tag>,
+pub(crate) fn emit_existing_tags<'a>(
+    tags: impl Iterator<Item = &'a Tag>,
     emit_stdout: bool,
     emit_fp: Option<&Path>,
 ) {
@@ -119,31 +119,23 @@ pub(crate) fn collect_tracks(
     emit_existing: bool,
     emit_existing_to: Option<PathBuf>,
 ) -> Vec<Track> {
-    let flac_files = source_dir
+    let track_paths = source_dir
         .read_dir()
         .unwrap()
         .map(|e| e.unwrap().path())
         .filter(|p| p.extension() == Some(OsStr::new("flac")))
         .collect::<Vec<_>>();
 
-    let mut expected_track_nums = (1..=flac_files.len()).collect::<HashSet<_>>();
-    let mut tracks = Vec::with_capacity(flac_files.len());
-    let mut tags_to_emit = None;
+    let mut expected_track_nums = (1..=track_paths.len()).collect::<HashSet<_>>();
 
-    if emit_existing || emit_existing_to.is_some() {
-        tags_to_emit = Some(Vec::with_capacity(flac_files.len()));
-    }
+    let mut tracks = Vec::with_capacity(track_paths.len());
 
-    for flac_file in flac_files {
-        println!("Found input file: {}", flac_file.display());
-        let flac_tag = Tag::read_from_path(&flac_file).unwrap();
+    for track_path in track_paths {
+        println!("Found input file: {}", track_path.display());
+        let track_tag = Tag::read_from_path(&track_path).unwrap();
 
-        let track_num_str = helpers::expect_one(flac_tag.get_vorbis("tracknumber").unwrap());
+        let track_num_str = helpers::expect_one(track_tag.get_vorbis("tracknumber").unwrap());
         let track_num = track_num_str.parse::<usize>().unwrap();
-
-        tags_to_emit.as_mut().map(|tte| {
-            tte.push((track_num, flac_tag));
-        });
 
         assert!(
             expected_track_nums.remove(&track_num),
@@ -152,7 +144,8 @@ pub(crate) fn collect_tracks(
 
         let track = Track {
             index: track_num,
-            path: flac_file,
+            path: track_path,
+            // tag: track_tag,
         };
 
         tracks.push(track);
@@ -162,20 +155,16 @@ pub(crate) fn collect_tracks(
     assert!(expected_track_nums.is_empty());
 
     // Sort the tracks by track number.
-    tracks.sort_by_key(|e| e.index);
+    tracks.sort_by_key(|t| t.index);
 
-    // Sort and emit existing tags, if requested.
-    tags_to_emit.as_mut().map(|tte| {
-        tte.sort_by_key(|(tn, _)| *tn);
-
-        let tags = tte.drain(..).map(|(_, tag)| tag);
-
-        emit_existing_tags(
-            tags,
-            emit_existing,
-            emit_existing_to.as_ref().map(|p| p.as_path()),
-        );
-    });
+    // Emit existing tags, if requested.
+    if emit_existing || emit_existing_to.is_some() {
+        // emit_existing_tags(
+        //     tracks.iter().map(|t| &t.tag),
+        //     emit_existing,
+        //     emit_existing_to,
+        // );
+    }
 
     tracks
 }
